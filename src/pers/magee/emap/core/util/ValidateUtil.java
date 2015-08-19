426174178319ece8d8dacaaca2139fc6c7278198
Magee_Yang <magee_yang@126.com>
2015-08-19 17:45:35 +0800

    d

--- src/pers/magee/emap/core/util/ValidateUtil.java
+++ src/pers/magee/emap/core/util/ValidateUtil.java
@@ -1,6 +1,12 @@
 package pers.magee.emap.core.util;
 
+import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ValidateUtil {
+	
+	private static final String EMAIL_PATTERN = "\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
 
 	/**
 	 * 判断字符串是否不为空
@@ -46,5 +52,45 @@ public class ValidateUtil {
 		}
 		return true;
 	}
+
+	/**
+	 * 判断字符串是否是日期格式，通过pattern进行日期格式的设置，默认是YYYYMMDD
+	 * @param str  需要判断的字符串
+	 * @param pattern  日期的转换格式
+	 * @return 如果转换成功则是true，转换异常或者str为null则为false
+	 */
+	public static boolean isDate(String str, String pattern) {
+		if (str == null) {
+			return false;
+		}
+		if (pattern == null) {
+			pattern = "YYYYMMDD";
+		}
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+		try{
+			sdf.parse(str);
+		}
+		catch(Exception e){
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * 判断字符串是否符合正则表达式的匹配要求
+	 * @param str 需要判断的字符串
+	 * @param pattern 正则表达式的字符串
+	 * @return 匹配则是true，不匹配则是false
+	 */
+	public static boolean isMatcher(String str,String pattern){
+		if(str==null){
+			return false;
+		}
+		Pattern pt = Pattern.compile(pattern);
+		Matcher mc = pt.matcher(str);
+		return mc.matches();
+	}
+	
+	
 
 }
